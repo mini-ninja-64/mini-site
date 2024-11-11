4232ff0a05b20deae015fd2ddc140efdc8ba8a4d
mini-ninja-64 <github@callumh.art>
2024-11-11 00:35:04 +0000

    Flesh out first post more

--- src/components/solid/Meowable.tsx
+++ src/components/solid/Meowable.tsx
@@ -1,8 +1,18 @@
-import type { ParentProps } from "solid-js";
+import { type ParentProps } from "solid-js";
 
 export default function Meowable({ children }: ParentProps) {
 	return (
-		<span style={{ cursor: "pointer" }} onClick={() => console.log("meow")}>
+		<span
+			style={{ cursor: "pointer" }}
+			onClick={() => {
+				const audio = new Audio("/audio/meow.mp3");
+				audio.preservesPitch = false;
+				audio.playbackRate = 1.0 - Math.random() * 0.25;
+				audio.play().catch((e) => console.error(e));
+			}}
+			// Prevent double click text selection
+			onMouseDown={(event) => event.preventDefault()}
+		>
 			{children}
 		</span>
 	);
