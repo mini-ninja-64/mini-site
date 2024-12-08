e850ed11ab4751b24b48adc6d96ba215ba8b2cd0
Mini <mini-ninja-64@users.noreply.github.com>
2024-12-08 23:09:01 +0000

    Add first post (#2)
    + Add icon support
    + Add file tree support
    + Add protonmail DNS config
    + Add note if there are no blog posts yet
    + Add embed colour
    + Add embed descriptions
    + Add image & audio exclusions to deployment action
    * Upgrade packages

--- src/components/solid/SolidComponent.tsx
+++ src/components/solid/SolidComponent.tsx
@@ -4,7 +4,10 @@ export default function SolidComponent() {
 	const [count, setCount] = createSignal(0);
 
 	return (
-		<div style={{ color: "aqua" }} onClick={() => setCount(count() + 1)}>
+		<div
+			style={{ color: "aqua", cursor: "pointer" }}
+			onClick={() => setCount(count() + 1)}
+		>
 			This is a Component: {count()} clicks
 		</div>
 	);
